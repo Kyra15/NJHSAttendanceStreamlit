67129302ac1db00dc868dd1ec53a72c8ffbff17f
Kyra15 <84361652+Kyra15@users.noreply.github.com>
2024-10-13 01:20:36 -0400

    Update style_funcs.py

--- style_funcs.py
+++ style_funcs.py
@@ -5,29 +5,29 @@ def hide_nav():
     st.markdown("<style> ul {display: none;} </style>", unsafe_allow_html=True)
 
 
-def hide_anchor_links():
-    st.markdown(
-        body="""
-        <style>
-            h1 > div > a {
-                display: none;
-            }
-            h2 > div > a {
-                display: none;
-            }
-            h3 > div > a {
-                display: none;
-            }
-            h4 > div > a {
-                display: none;
-            }
-            h5 > div > a {
-                display: none;
-            }
-            h6 > div > a {
-                display: none;
-            }
-        </style>
-        """,
-         unsafe_allow_html=True,
-)
+def sidebar():
+    st.sidebar.title("NJHS Attendance Program")
+
+    st.sidebar.page_link("streamlit_app.py", label="Home")
+    st.sidebar.page_link("pages/student_list.py", label="Student List")
+    st.sidebar.page_link("pages/program.py", label="Program")
+    st.sidebar.divider()
+    st.sidebar.header("Link to Code:", anchor=False)
+    st.sidebar.write("Made by Kyra Movva")
+    st.sidebar.link_button("Github Repo", "https://github.com/Kyra15/NJHSAttendanceStreamlit")
+    st.sidebar.write("")
+    st.sidebar.subheader("Email me if anything breaks!", anchor=False)
+    st.sidebar.link_button("kyra.movva@gmail.com", "mailto:kyra.movva@gmail.com")
+
+
+def formatting_for_str(str_format):
+    # print("before", str_format, type(str_format))
+    str_format = str_format.split(", ")
+    final_list = []
+    for i in str_format:
+        i = i.replace("[", "").replace("]", "").replace("'", "")
+        final_list.append(i)
+        # print(i)
+    # print("formatted", final_list, type(final_list))
+    final_str = "\n".join(final_list)
+    return final_str
