35b8e4719c07e14f7ea1f7b8a0b9c3b6c0f57636
Kyra15 <84361652+Kyra15@users.noreply.github.com>
2024-10-20 16:00:46 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,22 +11,27 @@ st.divider()
 st.header("Instructions", anchor=False)
 st.subheader("To set up program (only do for first use):", anchor=False)
 st.write("At the beginning of the year, you'll most likely get a list with every members' name. "
-         "Alphabetize that list, (look up a website for it if you need to) and copy it. "
-         "It should be in a format like the photo below.")
+         "Alphabetize that list and format it like the photo below.")
 
-st.columns(3)[1].image("/Users/kyramovva/NJHSAttendance/images/instruct1.jpeg", width=250)
-st.write("On this website, go to the \"Program\" section and look for the box that says \"Student List.\""
-         "Paste the list into that textbox.")
+st.image("images/student_input_ex.png", use_column_width="always")
 
+st.write("On this website, go to the \"Student List\" section."
+         "Paste the list into the input textbox and save it.")
 
-st.subheader("If program is already set up:")
+st.write("Now, on your google sheet (or make a copy if you want to be safe), "
+         "add this email address as an editor: \"njhs-sheet-editor@njhsattendence.iam.gserviceaccount.com\", "
+         " and set the viewing permission to \"Anyone with the link.\"")
+
+st.write("Back on this website, go to the program page and paste the link for this google sheet, then hit save.")
+
+st.subheader("If program is already set up:", anchor=False)
 st.write("Once you create your google form for the attendance and link that to a spreadsheet, "
          "export the sheet by going to File -> Download -> Comma Seperated Values (.csv).")
-st.columns(3)[1].image("/Users/kyramovva/NJHSAttendance/images/instruct1.jpeg", width=250)
+st.image("images/exporting_csv.gif", use_column_width="always")
 st.write("The csv file should have been saved to your device. "
          "Now, input the file into this website by going to the \"Program\" section and "
-         "dragging and dropping (computer only) the file into this website where it says \"Input File.\"")
-st.columns(3)[1].image("/Users/kyramovva/NJHSAttendance/images/instruct1.jpeg", width=250)
+         "inputting the file into this website.")
+st.image("images/uploading_file.gif", use_column_width="always")
 st.write("Click the \"Check Attendance\" button and wait until it finishes processing.")
 st.write("")
 st.write("If there are issues with the form, which will most likely come from typos or names not matching up, "
@@ -34,16 +39,10 @@ st.write("If there are issues with the form, which will most likely come from ty
          "Go back to the google sheet that your google form is connected to and fix the names with errors. "
          "After you've fixed the errors, redownload the file and try the program again. "
          "Keep repeating this until the \"Errors\" box is empty.")
-st.columns(3)[1].image("/Users/kyramovva/NJHSAttendance/images/instruct1.jpeg", width=250)
+st.image("images/fixing_errors.gif", use_column_width="always")
 st.write("The names of people who came to the meeting but aren't members of "
          "NJHS will also be outputted if you need them too :)")
 
-st.write("Once everything is good, click the \"Download Updated Attendance\" button. "
-         "The new file should be saved to your computer. Go to the Google Sheet link called \"NJHS Middleman.\" "
-         "Go to File -> Import and drag and drop the updated attendance file into there. Hit replace current sheet. "
-         "Now, copy the entire second column. Go to your main attendance spreadsheet (the one on schoology!) "
-         "and paste in the column.")
-
-st.columns(3)[1].image("/Users/kyramovva/NJHSAttendance/images/instruct1.jpeg", width=250)
+st.image("images/extras.png", use_column_width="always")
 st.subheader("That's it!", anchor=False)
 
