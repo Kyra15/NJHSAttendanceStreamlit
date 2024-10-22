074e8569185b15291e406a6956d6ae4aebf79841
Kyra15 <84361652+Kyra15@users.noreply.github.com>
2024-10-22 08:13:50 -0400

    Update student_list.py

--- pages/student_list.py
+++ pages/student_list.py
@@ -8,9 +8,7 @@ st.set_page_config(page_title='NJHS Attendance Program - Student List', layout="
 localS = LocalStorage()
 student_list = []
 
-localS.setItem("testing_key", "hello")
-st.write(localS.getItem("testimg_key"))
-    
+# I FIGURED IT OUT ITS JSON things aren't getting parsed in a json format
 
 if "student_names" not in st.session_state:
     st.session_state["student_names"] = []
@@ -25,14 +23,13 @@ st.header("Input:", anchor=False)
 with st.form("student_form"):
     st.write("Copy and paste names into here and click save when done")
     st.image("images/student_input_ex.png", use_column_width="always", caption="Example input")
-    #student_text = st.text_area("InputBox", value=formatting_for_str(str(localS.getItem("student_names"))),
-                                # label_visibility="collapsed", height=300)
+    student_text = st.text_area("InputBox", value=formatting_for_str(str(localS.getItem("student_names"))),
+                                label_visibility="collapsed", height=300)
     exists = st.form_submit_button("Save")
-    # if exists is not None:
-        # student_list = student_text.split("\n")
-        # localS.setItem("student_names", student_list)
-
-        # student_list = localS.getItem("student_names")
+    if exists is not None:
+        student_list = student_text.split("\n")
+        localS.setItem("student_names", student_list)
+        student_list = localS.getItem("student_names")
         # with open("masterlist.csv", "w", newline="") as f:
             # for i in student_list:
                 # if i != "":
