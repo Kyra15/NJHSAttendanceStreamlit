951bda7b01f7c309cde16184dba4939f5d7ddb58
Kyra15 <84361652+Kyra15@users.noreply.github.com>
2024-10-22 08:00:46 -0400

    Remove local storage

--- pages/student_list.py
+++ pages/student_list.py
@@ -5,7 +5,7 @@ from streamlit_local_storage import LocalStorage
 
 st.set_page_config(page_title='NJHS Attendance Program - Student List', layout="wide", page_icon='')
 
-localS = LocalStorage()
+# localS = LocalStorage()
 student_list = []
 
 if "student_names" not in st.session_state:
@@ -21,15 +21,15 @@ st.header("Input:", anchor=False)
 with st.form("student_form"):
     st.write("Copy and paste names into here and click save when done")
     st.image("images/student_input_ex.png", use_column_width="always", caption="Example input")
-    student_text = st.text_area("InputBox", value=formatting_for_str(str(localS.getItem("student_names"))),
+    #student_text = st.text_area("InputBox", value=formatting_for_str(str(localS.getItem("student_names"))),
                                 label_visibility="collapsed", height=300)
     exists = st.form_submit_button("Save")
-    if exists is not None:
-        student_list = student_text.split("\n")
-        localS.setItem("student_names", student_list)
+    # if exists is not None:
+        # student_list = student_text.split("\n")
+        # localS.setItem("student_names", student_list)
 
-        student_list = localS.getItem("student_names")
-        with open("masterlist.csv", "w", newline="") as f:
-            for i in student_list:
-                if i != "":
-                    f.write(i + "\n")
+        # student_list = localS.getItem("student_names")
+        # with open("masterlist.csv", "w", newline="") as f:
+            # for i in student_list:
+                # if i != "":
+                    # f.write(i + "\n")
